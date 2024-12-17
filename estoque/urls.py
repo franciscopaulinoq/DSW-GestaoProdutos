3127f6f8679d74a1d13dcf819699629cffde511a
Francisco Paulino <fqueiroz@consultores.reconcavo.org.br>
2024-12-16 21:13:37 -0300

    feat: adicionando página de detalhes de produtos

--- estoque/urls.py
+++ estoque/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ProdutosView, CategoriasView, FornecedoresView
+from .views import ProdutosView, ProdutoDetailsView, CategoriasView, FornecedoresView
 
 urlpatterns = [
     path('produtos/', ProdutosView, name="produtos"),
+    path('produto/<int:produto_id>', ProdutoDetailsView, name="produto-details"),
     path('categorias/', CategoriasView, name="categorias"),
     path('fornecedores/', FornecedoresView, name="fornecedores"),
 ]
--- estoque/views.py
+++ estoque/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Categoria, Fornecedor, Produto
 
 # Create your views here.
@@ -7,6 +7,11 @@ def ProdutosView(request):
     produtos = Produto.objects.all()
     context = {'produtos': produtos}
     return render(request, "produtos.html", context)
+
+def ProdutoDetailsView(request, produto_id):
+    produto = get_object_or_404(Produto, id=produto_id)
+    context = {'produto': produto}
+    return render(request, "produto-details.html", context)
 
 def CategoriasView(request):
     categorias = Categoria.objects.all()
